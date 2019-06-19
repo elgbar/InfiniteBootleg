3d85490d386397383c7119378255a5e776b98004
Elg <klink100@gmail.com>
2019-06-19 23:05:52 +0200

    Make player adhere to (some) collision

--- core/src/no/elg/infiniteBootleg/world/subgrid/Player.java
+++ core/src/no/elg/infiniteBootleg/world/subgrid/Player.java
@@ -1,6 +1,7 @@
 package no.elg.infiniteBootleg.world.subgrid;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import no.elg.infiniteBootleg.world.Block;
 import no.elg.infiniteBootleg.world.Material;
@@ -9,14 +10,16 @@ import org.jetbrains.annotations.NotNull;
 
 public class Player extends LivingEntity {
 
+    public static final float SPEED = 5f;
+
     private final TextureRegion region;
 
-    public Player(@NotNull World world, int x, int y) {
-        super(world, x, y);
+    public Player(@NotNull World world) {
+        super(world, world.getRender().getCamera().position.x / Block.BLOCK_SIZE,
+              world.getRender().getCamera().position.y / Block.BLOCK_SIZE);
         setFlying(true);
         region = new TextureRegion(Material.BRICK.getTextureRegion());
         region.flip(true, false);
-        System.out.println("player created");
     }
 
     @Override
@@ -28,6 +31,9 @@ public class Player extends LivingEntity {
     public void update() {
         super.update();
         Vector3 camPos = getWorld().getRender().getCamera().position;
-        getPosition().set(camPos.x / Block.BLOCK_SIZE, camPos.y / Block.BLOCK_SIZE);
+        //find the vector between the current pos and the camera pos
+        //to get the player to move the the correct direction negate the vector
+        Vector2 dv = getPosition().cpy().sub(camPos.x / Block.BLOCK_SIZE, camPos.y / Block.BLOCK_SIZE).scl(-SPEED);
+        getVelocity().set(dv);
     }
 }
