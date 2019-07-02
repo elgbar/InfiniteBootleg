b0893e459a3433d4cd394eb2f761bdd97a759b83
Karl Henrik Elg <klink100@gmail.com>
2019-07-02 18:48:52 +0200

    World ticker can now be paused

--- core/src/no/elg/infiniteBootleg/world/WorldTicker.java
+++ core/src/no/elg/infiniteBootleg/world/WorldTicker.java
@@ -2,6 +2,7 @@ package no.elg.infiniteBootleg.world;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.utils.PauseableThread;
 import com.strongjoshua.console.LogLevel;
 import no.elg.infiniteBootleg.Main;
 import org.jetbrains.annotations.NotNull;
@@ -20,49 +21,46 @@ public class WorldTicker {
     public static final long TICKS_PER_SECOND = 60L;
     public static final long MS_DELAY_BETWEEN_TICKS = 1000L / TICKS_PER_SECOND;
     public static final float SECONDS_DELAY_BETWEEN_TICKS = 1f / TICKS_PER_SECOND;
+    private final PauseableThread worldTickThread;
 
     private long tickId;
     private long frameId;
 
     private long stuckFrames;
-    private boolean running = true;
 
     public WorldTicker(@NotNull World world) {
         Main.inst().getConsoleLogger().logf("TPS: %d", TICKS_PER_SECOND);
-        Thread worldTickThread = new Thread("World tick thread") {
-            @Override
-            public void run() {
-                try {
-                    while (running) {
-                        if (frameId != Gdx.graphics.getFrameId()) {
+        worldTickThread = new PauseableThread(() -> {
+            try {
+                if (frameId != Gdx.graphics.getFrameId()) {
 //                            if (tickId % TICKS_PER_SECOND == 0) {
 ////                                System.out.println("tick: " + tickId);
 //                            }
-                            //force save every 30 sec, but not first tick
-                            //FIXME not currently working (not writing chunks that hasn't been modified since last times
+                    //force save every 30 sec, but not first tick
+                    //FIXME not currently working (not writing chunks that hasn't been modified since last times
 //                            if (tickId > 0 && tickId % (TICKS_PER_SECOND * 10) == 0) {
 //                                System.out.println("Saving world " + world.getName() + " (" + world.getUuid() + ")");
 //                                Gdx.app.postRunnable(world::save);
 //                            }
-                            Gdx.app.postRunnable(world::update);
-                            tickId++;
-                            stuckFrames = 0;
-                            frameId = Gdx.graphics.getFrameId();
-                        }
-                        else {
-                            stuckFrames++;
-                            if (stuckFrames == TICKS_PER_SECOND || stuckFrames % (TICKS_PER_SECOND * 10) == 0) {
-                                Main.inst().getConsoleLogger()
-                                    .logf(LogLevel.ERROR, "Can't keep up! Failed to update world for %d ticks", stuckFrames);
-                            }
-                        }
-                        Thread.sleep(MS_DELAY_BETWEEN_TICKS);
-                    }
-                } catch (InterruptedException ignored) {
-                    Main.inst().getConsoleLogger().log("World updater interrupted");
+                    Gdx.app.postRunnable(world::update);
+                    tickId++;
+                    stuckFrames = 0;
+                    frameId = Gdx.graphics.getFrameId();
                 }
+                else {
+                    stuckFrames++;
+                    if (stuckFrames == TICKS_PER_SECOND || stuckFrames % (TICKS_PER_SECOND * 10) == 0) {
+                        Main.inst().getConsoleLogger()
+                            .logf(LogLevel.ERROR, "Can't keep up! Failed to update world for %d ticks", stuckFrames);
+                    }
+                }
+                Thread.sleep(MS_DELAY_BETWEEN_TICKS);
+            } catch (InterruptedException ignored) {
+                Main.inst().getConsoleLogger().log("World updater interrupted");
             }
-        };
+
+        });
+        worldTickThread.setName("World tick thread");
         worldTickThread.setDaemon(true);
         worldTickThread.start();
     }
@@ -78,6 +76,14 @@ public class WorldTicker {
      * Stop this world ticker, world's {@link World#update()} method will no longer be called
      */
     public void stop() {
-        running = false;
+        worldTickThread.stopThread();
+    }
+
+    public void pause() {
+        worldTickThread.onPause();
+    }
+
+    public void resume() {
+        worldTickThread.onResume();
     }
 }
