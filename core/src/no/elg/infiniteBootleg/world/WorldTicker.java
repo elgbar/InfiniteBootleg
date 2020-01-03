d00116d1a95f3de96647fe3d8a7081609c41e0de
Elg <klink100@gmail.com>
2020-01-03 11:03:55 +0100

    Reduce tps to 20 and complain about the tps less often

--- core/src/no/elg/infiniteBootleg/world/WorldTicker.java
+++ core/src/no/elg/infiniteBootleg/world/WorldTicker.java
@@ -18,17 +18,23 @@ import org.jetbrains.annotations.NotNull;
  */
 public class WorldTicker implements Runnable {
 
-    public static final long TICKS_PER_SECOND = 30L;
+    public static final long TICKS_PER_SECOND = 20L;
     public static final long MS_DELAY_BETWEEN_TICKS = 1000L / TICKS_PER_SECOND;
     public static final long NANO_DELAY_BETWEEN_TICKS = 1_000_000_000L / TICKS_PER_SECOND;
     public static final float SECONDS_DELAY_BETWEEN_TICKS = 1f / TICKS_PER_SECOND;
+
+    public static final long NAG_DELAY_TICKS = TICKS_PER_SECOND * 3;
+
     private final PauseableThread worldTickThread;
     private final World world;
 
-    private long tickId;
-    private long tps = -1;
-    private long startTime;
-    private long tpsTick;
+    private long tickId;            //The current tick
+    private long tps = -1;          //The current ticks per second
+    private long tpsCalcStartTime;  //At what tick did we start the tps calculation
+    private long tpsTick;           //Ticks accumulated during calculating tps
+    private long tpsDelta;
+
+    private long lastNagged; //How long ago we last showed a 'cant keep up' warning
 
     public WorldTicker(@NotNull World world, boolean start) {
         this.world = world;
@@ -58,28 +64,29 @@ public class WorldTicker implements Runnable {
         tickId++;
 
         //Calculate ticks per second
-        if (start - startTime >= 1000000000) {
+        if (start - tpsCalcStartTime >= 1000000000) {
             tps = tpsTick;
             tpsTick = 0;
-            startTime = start;
+            tpsCalcStartTime = start;
         }
         tpsTick++;
 
-        long totalNanos = TimeUtils.nanoTime() - start;
-        long ms = MS_DELAY_BETWEEN_TICKS - TimeUtils.nanosToMillis(totalNanos);
+        tpsDelta = TimeUtils.nanoTime() - start;
+        long ms = MS_DELAY_BETWEEN_TICKS - TimeUtils.nanosToMillis(tpsDelta);
 
         if (ms > 0) {
             try {
-                int nano = (int) (totalNanos % 1_000_000); // There are one million nano second in a millisecond
+                int nano = (int) tpsDelta % 1_000_000; // There are one million nano second in a millisecond
                 Thread.sleep(ms, nano);
             } catch (InterruptedException e) {
                 Main.logger().error("TICK", "World ticker interrupted");
             }
         }
-        else {
-            Main.logger().error("TICK",
-                                "Cant keep up a single tick took around " + TimeUtils.nanosToMillis(totalNanos) +
-                                " ms, while at max it should take " + MS_DELAY_BETWEEN_TICKS + " ms");
+        else if (tickId - lastNagged >= NAG_DELAY_TICKS) {
+
+            lastNagged = tickId;
+            Main.logger().error("TICK", "Cant keep up a single tick took around " + TimeUtils.nanosToMillis(tpsDelta) +
+                                        " ms, while at max it should take " + MS_DELAY_BETWEEN_TICKS + " ms");
         }
     }
 
@@ -92,8 +99,18 @@ public class WorldTicker implements Runnable {
     }
 
 
+    /**
+     * @return The current TPS might differ from {@link #TICKS_PER_SECOND} but will never be greater than it
+     */
     public long getRealTPS() {
         return tps;
+    }
+
+    /**
+     * @return Nanoseconds between each tick
+     */
+    public long getTpsDelta() {
+        return tpsDelta;
     }
 
     /**
