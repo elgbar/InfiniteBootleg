482d3bf68246eb694e8772ec9e92bdd738fe2a3b
Karl Henrik Elg <klink100@gmail.com>
2020-01-07 20:59:01 +0100

    Change debug chunk colors

--- core/src/no/elg/infiniteBootleg/world/render/DebugChunkRenderer.java
+++ core/src/no/elg/infiniteBootleg/world/render/DebugChunkRenderer.java
@@ -2,7 +2,6 @@ package no.elg.infiniteBootleg.world.render;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import no.elg.infiniteBootleg.Main;
 import no.elg.infiniteBootleg.Renderer;
@@ -12,16 +11,17 @@ import no.elg.infiniteBootleg.world.Chunk;
 
 public class DebugChunkRenderer implements Renderer {
 
+    public static final Color WITHIN_CAMERA_COLOR = Color.TEAL;
+    public static final Color OUTSIDE_CAMERA_COLOR = Color.FIREBRICK;
+
     private final WorldRender worldRender;
     private final ShapeRenderer lr;
     private final OrthographicCamera camera;
-    private final SpriteBatch batch;
 
     public DebugChunkRenderer(WorldRender worldRender) {
         this.worldRender = worldRender;
         camera = worldRender.getCamera();
         lr = new ShapeRenderer(1000);
-        batch = new SpriteBatch();
     }
 
     @Override
@@ -32,7 +32,6 @@ public class DebugChunkRenderer implements Renderer {
         int yEnd = chunksInView.vertical_end;
         int xEnd = chunksInView.horizontal_end;
 
-
         lr.begin(ShapeRenderer.ShapeType.Line);
         lr.setProjectionMatrix(camera.combined);
 
@@ -42,10 +41,10 @@ public class DebugChunkRenderer implements Renderer {
                 Color c;
                 if (y == chunksInView.vertical_end - 1 || x == chunksInView.horizontal_start ||
                     x == chunksInView.horizontal_end - 1) {
-                    c = Color.GOLDENROD;
+                    c = OUTSIDE_CAMERA_COLOR;
                 }
                 else {
-                    c = Color.FOREST;
+                    c = WITHIN_CAMERA_COLOR;
                 }
                 lr.setColor(c);
                 lr.rect(x * offset + 0.5f, y * offset + 0.5f, offset - 1, offset - 1);
@@ -56,10 +55,10 @@ public class DebugChunkRenderer implements Renderer {
         ScreenRenderer sr = Main.inst().getScreenRenderer();
         sr.begin();
         sr.drawBottom("Debug Chunk outline legend", 5);
-        sr.getFont().setColor(Color.FOREST);
-        sr.drawBottom("  Green are chunks that you see", 3);
-        sr.getFont().setColor(Color.GOLDENROD);
-        sr.drawBottom("  Orange means only the physics are active", 1);
+        sr.getFont().setColor(WITHIN_CAMERA_COLOR);
+        sr.drawBottom("  Chunks within the camera boarders", 3);
+        sr.getFont().setColor(OUTSIDE_CAMERA_COLOR);
+        sr.drawBottom("  Chunks outside camera boarders, only physics active", 1);
         sr.end();
         sr.resetFontColor();
     }
