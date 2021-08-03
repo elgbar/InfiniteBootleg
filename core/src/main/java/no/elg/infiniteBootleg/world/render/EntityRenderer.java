9de7cbf61e3c4e862faf87355a73be666f533191
Elg <karl.henrik.elg.barlinn@vy.no>
2021-08-03 12:16:57 +0200

    Fix potential NPE

--- core/src/main/java/no/elg/infiniteBootleg/world/render/EntityRenderer.java
+++ core/src/main/java/no/elg/infiniteBootleg/world/render/EntityRenderer.java
@@ -1,6 +1,7 @@
 package no.elg.infiniteBootleg.world.render;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import no.elg.infiniteBootleg.Renderer;
 import static no.elg.infiniteBootleg.world.Block.BLOCK_SIZE;
 import no.elg.infiniteBootleg.world.subgrid.Entity;
@@ -19,10 +20,11 @@ public class EntityRenderer implements Renderer {
     public void render() {
         Batch batch = worldRender.getBatch();
         for (Entity entity : worldRender.getWorld().getEntities()) {
-            if (entity.getTextureRegion() == null) { continue; }
+            TextureRegion textureRegion = entity.getTextureRegion();
+            if (textureRegion == null) { continue; }
             float x = (entity.getPosition().x - entity.getHalfBox2dWidth()) * BLOCK_SIZE;
             float y = (entity.getPosition().y - entity.getHalfBox2dHeight()) * BLOCK_SIZE;
-            batch.draw(entity.getTextureRegion(), x, y, entity.getWidth(), entity.getHeight());
+            batch.draw(textureRegion, x, y, entity.getWidth(), entity.getHeight());
         }
     }
 }
