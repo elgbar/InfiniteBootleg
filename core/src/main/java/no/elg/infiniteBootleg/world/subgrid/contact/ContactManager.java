cc9a64c342387164317c366b76756eb434983d25
Elg <karl.henrik.elg.barlinn@vy.no>
2021-08-06 19:32:14 +0200

    Synchronize and optimize contact manager

--- core/src/main/java/no/elg/infiniteBootleg/world/subgrid/contact/ContactManager.java
+++ core/src/main/java/no/elg/infiniteBootleg/world/subgrid/contact/ContactManager.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import no.elg.infiniteBootleg.world.World;
+import static no.elg.infiniteBootleg.world.render.WorldRender.BOX2D_LOCK;
 import no.elg.infiniteBootleg.world.subgrid.Entity;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,22 +17,26 @@ public class ContactManager implements ContactListener {
         this.world = world;
     }
 
-    @Override
-    public void beginContact(Contact contact) {
-        for (Entity entity : world.getEntities()) {
-            if (!entity.isInvalid() && contact.getFixtureB().getBody() == entity.getBody()) {
-                entity.contact(ContactType.BEGIN_CONTACT, contact);
+    private void handleContact(ContactType type, Contact contact) {
+        //Must be within a box2d lock to make sure the contact is valid
+        synchronized (BOX2D_LOCK) {
+            for (Entity entity : world.getEntities()) {
+                if (!entity.isInvalid() && contact.getFixtureB().getBody() == entity.getBody()) {
+                    entity.contact(type, contact);
+                    break; //Assume each body is unique to an entity
+                }
             }
         }
     }
 
     @Override
+    public void beginContact(Contact contact) {
+        handleContact(ContactType.BEGIN_CONTACT, contact);
+    }
+
+    @Override
     public void endContact(Contact contact) {
-        for (Entity entity : world.getEntities()) {
-            if (!entity.isInvalid() && contact.getFixtureB().getBody() == entity.getBody()) {
-                entity.contact(ContactType.END_CONTACT, contact);
-            }
-        }
+        handleContact(ContactType.END_CONTACT, contact);
     }
 
     @Override
