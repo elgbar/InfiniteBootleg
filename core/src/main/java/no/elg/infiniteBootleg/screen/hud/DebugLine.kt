6829346a1e96878da16e9b1ea4fa01e21ccce0d4
Karl Henrik Elg <klink100@gmail.com>
2022-04-13 02:40:21 +0200

    Improve fpsString and convert DebugLine to kotlin

---
+++ core/src/main/java/no/elg/infiniteBootleg/screen/hud/DebugLine.kt
@@ -0,0 +1,142 @@
+package no.elg.infiniteBootleg.screen.hud
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.utils.TimeUtils
+import no.elg.infiniteBootleg.ClientMain
+import no.elg.infiniteBootleg.util.CoordUtil
+import no.elg.infiniteBootleg.util.fastIntFormat
+import no.elg.infiniteBootleg.world.Chunk
+import no.elg.infiniteBootleg.world.ClientWorld
+import no.elg.infiniteBootleg.world.Material
+import no.elg.infiniteBootleg.world.subgrid.LivingEntity
+import java.text.DecimalFormat
+
+object DebugLine {
+
+  val dotFourFormat = DecimalFormat("0.0000")
+
+  @JvmStatic
+  inline fun fpsString(sb: StringBuilder, world: ClientWorld?) {
+    val worldTicker = world?.worldTicker
+    val tpsDelta = if (worldTicker == null) -1 else TimeUtils.nanosToMillis(worldTicker.tpsDelta)
+    val realTPS = worldTicker?.realTPS ?: -1
+
+    sb.append("FPS: ").fastIntFormat(Gdx.graphics.framesPerSecond, 4)
+      .append(" delta: ").append(dotFourFormat.format(Gdx.graphics.deltaTime.toDouble()))
+      .append(" tps: ").fastIntFormat(realTPS.toInt(), 2)
+      .append(" tps delta: ").fastIntFormat(tpsDelta.toInt(), 3)
+//      .append(" ms active threads ").append(Main.inst().scheduler.activeThreads)
+  }
+
+  @JvmStatic
+  fun lights(sb: StringBuilder, world: ClientWorld) {
+    sb.append("Active Lights: ")
+    val handler = world.render.rayHandler
+    sb.append(handler.enabledLights.size)
+  }
+
+  @JvmStatic
+  fun pointing(
+    sb: StringBuilder,
+    world: ClientWorld,
+    mouseBlockX: Int,
+    mouseBlockY: Int
+  ) {
+    val block = world.getBlock(mouseBlockX, mouseBlockY, true)
+    val material = block?.material ?: Material.AIR
+    val rawX = ClientMain.inst().mouseX
+    val rawY = ClientMain.inst().mouseY
+    val exists = block != null
+    val blockDebug = block?.hudDebug() ?: ""
+    val format = "Pointing at %-5s (% 8.2f,% 8.2f) block (% 5d,% 5d) exists? %-5b %s"
+    sb.append(String.format(format, material, rawX, rawY, mouseBlockX, mouseBlockY, exists, blockDebug))
+  }
+
+  @JvmStatic
+  fun chunk(
+    sb: StringBuilder,
+    world: ClientWorld,
+    mouseBlockX: Int,
+    mouseBlockY: Int
+  ) {
+    val pc = world.getChunkFromWorld(mouseBlockX, mouseBlockY)
+    val chunkY = CoordUtil.worldToChunk(mouseBlockY)
+    val chunkX = CoordUtil.worldToChunk(mouseBlockX)
+    if (pc == null) {
+      val format = "chunk (% 4d,% 4d) : not loaded"
+      sb.append(String.format(format, chunkX, chunkY))
+    } else {
+      val biome = world.chunkLoader.generator.getBiome(mouseBlockX)
+      val allAir = pc.isAllAir
+      val allowUnloading = pc.isAllowingUnloading
+      val format = "chunk (% 4d,% 4d) : type: %-9.9s just air? %-5b can unload? %-5b"
+      sb.append(String.format(format, chunkX, chunkY, biome, allAir, allowUnloading))
+    }
+  }
+
+  @JvmStatic
+  fun time(sb: StringBuilder, world: ClientWorld) {
+    val worldTime = world.worldTime
+    val format = "time: %.2f (%.2f) scale: %.2f sky brightness: %.2f TOD: %s"
+    sb.append(
+      String.format(
+        format,
+        worldTime.time,
+        worldTime.normalizedTime(),
+        worldTime.timeScale,
+        worldTime.skyBrightness,
+        worldTime.timeOfDay(worldTime.time)
+      )
+    )
+  }
+
+  @JvmStatic
+  fun viewChunk(sb: StringBuilder, world: ClientWorld) {
+    val viewingChunks = world.render.chunksInView
+    val chunksHor = viewingChunks.horizontalLength
+    val chunksVer = viewingChunks.verticalLength
+    val chunksInView = chunksHor * chunksVer
+    val blocks = chunksInView * Chunk.CHUNK_SIZE * Chunk.CHUNK_SIZE
+    val blocksHor = chunksHor * Chunk.CHUNK_SIZE
+    val blocksVer = chunksVer * Chunk.CHUNK_SIZE
+    val zoom = world.render.camera.zoom
+    val format = "Viewing %d chunks (total %d blocks, w %d b, h %d b) with zoom: %.3f"
+    sb.append(String.format(format, chunksInView, blocks, blocksHor, blocksVer, zoom))
+  }
+
+  @JvmStatic
+  fun pos(sb: StringBuilder, player: LivingEntity?) {
+    if (player == null) {
+      sb.append("No player")
+      return
+    }
+    val velocity = player.velocity
+    val position = player.position
+    val physicsPosition = player.physicsPosition
+    val onGround = player.isOnGround
+    val flying = player.isFlying
+    val format = "p: (% 8.2f,% 8.2f) v: (% 8.2f,% 8.2f) php: (% 8.2f,% 8.2f) g? %-5b f? %-5b"
+    sb.append(
+      String.format(
+        format,
+        position.x,
+        position.y,
+        velocity.x,
+        velocity.y,
+        physicsPosition.x,
+        physicsPosition.y,
+        onGround,
+        flying
+      )
+    )
+  }
+
+  @JvmStatic
+  fun ents(sb: StringBuilder, world: ClientWorld) {
+    val nl = "\n    "
+    sb.append("E = ")
+    for (entity in world.getEntities(ClientMain.inst().mouseX, ClientMain.inst().mouseY)) {
+      sb.append(entity.simpleName()).append("[").append(entity.hudDebug()).append("]").append(nl)
+    }
+  }
+}
--- core/src/main/kotlin/no/elg/infiniteBootleg/util/Extra.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/util/Extra.kt
@@ -17,3 +17,41 @@ private val uuidv5Generator = Generators.nameBasedGenerator(namespace)
 fun generateUUIDFromName(string: String): UUID {
   return uuidv5Generator.generate(string)
 }
+
+fun StringBuilder.fastIntFormat(int: Int, d: Byte): StringBuilder {
+  val len = int.stringSize()
+  this.ensureCapacity(d.toInt())
+  if (len < d) {
+    for (i in 1..(d - len)) {
+      this.append(" ")
+    }
+  }
+  append(int)
+  return this
+}
+
+/**
+ * Returns the string representation size for a given int value.
+ *
+ * @param x int value
+ * @return string size
+ *
+ * @implNote There are other ways to compute this: e.g. binary search,
+ * but values are biased heavily towards zero, and therefore linear search
+ * wins. The iteration results are also routinely inlined in the generated
+ * code after loop unrolling.
+ */
+fun Int.stringSize(): Int {
+  var x = this
+  var d = 1
+  if (x >= 0) {
+    d = 0
+    x = -x
+  }
+  var p = -10
+  for (i in 1..9) {
+    if (x > p) return i + d
+    p *= 10
+  }
+  return 10 + d
+}
