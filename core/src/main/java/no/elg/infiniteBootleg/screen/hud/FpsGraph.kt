e06a5efdfd47bbe0525c103052f3b7001e59bd89
Karl Henrik Elg <klink100@gmail.com>
2022-04-14 18:28:37 +0200

    Re-implement the FPS graph with a frame buffer Several improvements have been made * The width of each frame column is customizable * Reduced number of colors to make it clear how bad or good the fps is * Performance is tenfold, from taking up ~56% of all CPU time to ~5.8%

--- core/src/main/java/no/elg/infiniteBootleg/screen/hud/FpsGraph.kt
+++ core/src/main/java/no/elg/infiniteBootleg/screen/hud/FpsGraph.kt
@@ -2,58 +2,71 @@ package no.elg.infiniteBootleg.screen.hud
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Pixmap
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.glutils.FrameBuffer
 import no.elg.infiniteBootleg.screen.ScreenRenderer
 import no.elg.infiniteBootleg.util.Resizable
 
 object FpsGraph : Resizable {
 
-  private lateinit var pixmap: Pixmap
-  private lateinit var texture: Texture
+  private lateinit var fbo: FrameBuffer
+
   private var width: Int = 0
   private var height: Int = 0
   private var index: Int = 0
+  private const val COL_WIDTH = 2
+  private const val COLOR_SIZE = 6
 
-  init {
-    resize(Gdx.graphics.width, Gdx.graphics.height)
+  private val colors = Array<Color>(COLOR_SIZE) {
+    when (it) {
+      0 -> Color.GREEN
+      1 -> Color.LIME
+      2 -> Color.YELLOW
+      3 -> Color.GOLD
+      4 -> Color.ORANGE
+      5 -> Color.RED
+      else -> Color.BLUE
+    }
   }
 
   fun render(sr: ScreenRenderer) {
     newFrame()
-    texture.load(texture.textureData)
-    sr.batch.draw(texture, 0f, 0f)
+    sr.batch.draw(fbo.colorBufferTexture, 0f, 0f)
   }
 
   private fun newFrame() {
     if (index >= width) {
       index = 0
     } else {
-      index++
+      index += COL_WIDTH
     }
 
     val delta = Gdx.graphics.deltaTime * 10_000
-    val pillarHeight = height - delta.toInt()
+    val pillarHeight = (height - delta.toInt()).coerceAtLeast(0)
 
-    pixmap.setColor(Color.CLEAR)
-    pixmap.drawLine(index, 0, index, pillarHeight)
+    val colorIndex = (delta / height * COLOR_SIZE.toFloat()).toInt()
+    val color = colors.getOrNull(colorIndex) ?: Color.RED
 
-    pixmap.setColor(Color(0.1f + (delta / height), 0.9f - (delta / height), 0f, 1f))
-    pixmap.drawLine(index, height, index, pillarHeight)
-  }
+    fbo.begin()
 
-  private fun clear() {
-    pixmap.setColor(Color.CLEAR)
-    pixmap.fill()
-    pixmap.setColor(Color.GREEN)
-  }
+    // https://www.khronos.org/opengl/wiki/Scissor_Test
+    Gdx.gl.glEnable(GL20.GL_SCISSOR_TEST)
 
-  private fun buffer(): Pixmap {
-    val pixmap = Pixmap(width, height, Pixmap.Format.RGBA4444)
-    pixmap.blending = Pixmap.Blending.None
-    pixmap.filter = Pixmap.Filter.NearestNeighbour
+    // 0 is top, height is bottom, pillarHeight is how far up the pillar should go
+    // Fill from the top to pillarHeight with transparent pixels
+    Gdx.gl.glScissor(index, 0, COL_WIDTH + 1, height)
+    Gdx.gl.glClearColor(0f, 0f, 0f, 0f)
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-    return pixmap
+    // Then fill the rest (from pillarHeight to bottom) with colored pixels
+    Gdx.gl.glScissor(index, pillarHeight, COL_WIDTH, height)
+    Gdx.gl.glClearColor(color.r, color.g, color.b, 1f)
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+    Gdx.gl.glDisable(GL20.GL_SCISSOR_TEST)
+
+    fbo.end()
   }
 
   override fun resize(width: Int, height: Int) {
@@ -61,15 +74,9 @@ object FpsGraph : Resizable {
     FpsGraph.height = height / 3
     index = Int.MAX_VALUE
 
-    if (::pixmap.isInitialized) {
-      pixmap.dispose()
+    if (::fbo.isInitialized) {
+      fbo.dispose()
     }
-    if (::texture.isInitialized) {
-      texture.dispose()
-    }
-    pixmap = buffer()
-    texture = Texture(pixmap)
-
-    clear()
+    fbo = FrameBuffer(Pixmap.Format.RGBA4444, FpsGraph.width, FpsGraph.height, false)
   }
 }
