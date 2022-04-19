c14cfab963508d3f60b01e0ac31c938003dce941
Karl Henrik Elg <klink100@gmail.com>
2022-04-19 20:18:38 +0200

    Fix calling client main when server

--- core/src/main/java/no/elg/infiniteBootleg/world/World.java
+++ core/src/main/java/no/elg/infiniteBootleg/world/World.java
@@ -198,7 +198,7 @@ public abstract class World implements Disposable, Resizable {
         }
       }
     }
-    if (ClientMain.inst().isSinglePlayer() && ClientMain.inst().getPlayer() == null) {
+    if (Main.isSingleplayer() && ClientMain.inst().getPlayer() == null) {
       ClientMain.inst().setPlayer(new Player(this, spawn.x, spawn.y));
     }
     getRender().update();
