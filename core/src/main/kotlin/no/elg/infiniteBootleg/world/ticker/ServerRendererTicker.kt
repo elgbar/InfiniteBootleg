55c77a6b4bdb58edbadc666c0be542476277ba61
Karl Henrik Elg <klink100@gmail.com>
2022-07-22 01:13:48 +0200

    Increase server rendering tps

--- core/src/main/kotlin/no/elg/infiniteBootleg/world/ticker/ServerRendererTicker.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/ticker/ServerRendererTicker.kt
@@ -6,7 +6,7 @@ import no.elg.infiniteBootleg.world.ServerWorld
 
 class ServerRendererTicker(private val world: ServerWorld, tick: Boolean) : Ticking {
 
-  val ticker: Ticker = Ticker(this, "ServerRender-" + world.name, tick, 1, Double.MAX_VALUE)
+  val ticker: Ticker = Ticker(this, "ServerRender-" + world.name, tick, 10, Double.MAX_VALUE)
 
   override fun tick() {
     world.render.update()
