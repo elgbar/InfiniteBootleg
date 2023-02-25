37959aa22c8e42b60c1556642af5a28d01712654
Karl Henrik Elg <klink100@gmail.com>
2023-02-25 16:23:10 +0100

    Only create events for input which are handled

--- core/src/main/kotlin/no/elg/infiniteBootleg/input/ECSInputListener.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/input/ECSInputListener.kt
@@ -18,19 +18,19 @@ class ECSInputListener(val world: World) : InputProcessor, Disposable {
 
   override fun keyDown(keycode: Int): Boolean = handleEvent(InputEvent.KeyDownEvent(keycode))
 
-  override fun keyUp(keycode: Int): Boolean = handleEvent(InputEvent.KeyUpEvent(keycode))
+  override fun keyUp(keycode: Int): Boolean = false // handleEvent(InputEvent.KeyUpEvent(keycode))
 
-  override fun keyTyped(character: Char): Boolean = handleEvent(InputEvent.KeyTypedEvent(character))
+  override fun keyTyped(character: Char): Boolean = false // handleEvent(InputEvent.KeyTypedEvent(character))
 
   override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = handleEvent(InputEvent.TouchDownEvent(screenX, screenY, pointer, button))
 
-  override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = handleEvent(InputEvent.TouchUpEvent(screenX, screenY, pointer, button))
+  override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = false // handleEvent(InputEvent.TouchUpEvent(screenX, screenY, pointer, button))
 
-  override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean = handleEvent(InputEvent.TouchDraggedEvent(screenX, screenY, pointer))
+  override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean = false // handleEvent(InputEvent.TouchDraggedEvent(screenX, screenY, pointer))
 
-  override fun mouseMoved(screenX: Int, screenY: Int): Boolean = handleEvent(InputEvent.MouseMovedEvent(screenX, screenY))
+  override fun mouseMoved(screenX: Int, screenY: Int): Boolean = false // handleEvent(InputEvent.MouseMovedEvent(screenX, screenY))
 
-  override fun scrolled(amountX: Float, amountY: Float): Boolean = handleEvent(InputEvent.ScrolledEvent(amountX, amountY))
+  override fun scrolled(amountX: Float, amountY: Float): Boolean = false // handleEvent(InputEvent.ScrolledEvent(amountX, amountY))
 
   override fun dispose() {
     ClientMain.inst().inputMultiplexer.removeProcessor(this)
