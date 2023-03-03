c892955ceb81c67997288d0d5d6dc04d57fc6e44
Karl Henrik Elg Barlinn <1556738+elgbar@users.noreply.github.com>
2023-03-03 08:34:38 +0100

    Re-double the gravity scale

--- core/src/main/java/no/elg/infiniteBootleg/world/Constants.kt
+++ core/src/main/java/no/elg/infiniteBootleg/world/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
   const val GROUND_CHECK_OFFSET = 0.1f
   const val TELEPORT_DIFFERENCE_THRESHOLD = .5f
   const val TELEPORT_DIFFERENCE_Y_OFFSET = 0.01f
-  const val DEFAULT_GRAVITY_SCALE = 1f
+  const val DEFAULT_GRAVITY_SCALE = 2f
   const val FREEZE_DESPAWN_TIMEOUT_MS = 1000L
   const val MIN_CHECK_STEP = 0.1f
   const val DISPOSED_SANITY_CHECK_DELAY = 10L
