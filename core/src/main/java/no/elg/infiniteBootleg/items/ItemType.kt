51252c8a85903826e28d95cd69d346f8c2118cd9
Karl Henrik Elg <klink100@gmail.com>
2023-06-21 19:01:46 +0200

    Convert item type to kotlin

---
+++ core/src/main/java/no/elg/infiniteBootleg/items/ItemType.kt
@@ -0,0 +1,12 @@
+package no.elg.infiniteBootleg.items
+
+enum class ItemType {
+  /** It's air, the stuff you breath  */
+  AIR,
+
+  /** A block in the world  */
+  BLOCK,
+
+  /** Something that can move between blocks and have a Box2D body  */
+  ENTITY
+}
