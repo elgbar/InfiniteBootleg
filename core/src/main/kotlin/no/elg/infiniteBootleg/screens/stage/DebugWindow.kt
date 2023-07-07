68bfe5c07c4ab15a7256cd181b2fc99152ef67e2
Karl Henrik Elg <klink100@gmail.com>
2023-07-07 09:38:38 +0200

    Add tooltip to debug menu

--- core/src/main/kotlin/no/elg/infiniteBootleg/screens/stage/DebugWindow.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/screens/stage/DebugWindow.kt
@@ -1,9 +1,12 @@
 package no.elg.infiniteBootleg.screens.stage
 
-import ktx.actors.onClickEvent
+import com.kotcrab.vis.ui.widget.VisLabel
+import ktx.actors.onClick
 import ktx.scene2d.Scene2dDsl
 import ktx.scene2d.vis.KVisWindow
 import ktx.scene2d.vis.visTextButton
+import ktx.scene2d.vis.visTextTooltip
+import no.elg.hex.util.toAbled
 
 @Scene2dDsl
 fun KVisWindow.toggleableDebugButton(
@@ -13,9 +16,16 @@ fun KVisWindow.toggleableDebugButton(
 ) = visTextButton(name, style = "debug-menu-button") {
   pad(5f)
   isDisabled = initiallyDisabled
-  onClickEvent { event ->
-    event.cancel()
+
+  val tooltipLabel: VisLabel
+  fun tooltipText() = "$name is ${isDisabled.toAbled()}"
+  visTextTooltip(tooltipText()) {
+    tooltipLabel = this.content as VisLabel
+  }
+
+  onClick {
     isDisabled = !isDisabled
+    tooltipLabel.setText(tooltipText())
     onToggle()
   }
   it.fillX()
