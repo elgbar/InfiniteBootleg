4010917b2b909f135e5134f2de0552a57c2e93f4
Karl Henrik Elg <klink100@gmail.com>
2023-07-13 20:31:38 +0200

    Catch NPE when rendering

--- core/src/main/kotlin/no/elg/infiniteBootleg/screens/hud/CurrentBlockHUDRenderer.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/screens/hud/CurrentBlockHUDRenderer.kt
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.utils.Align
 import no.elg.infiniteBootleg.ClientMain
 import no.elg.infiniteBootleg.KAssets
+import no.elg.infiniteBootleg.Main
 import no.elg.infiniteBootleg.items.Item
 import no.elg.infiniteBootleg.screen.ScreenRenderer
 import no.elg.infiniteBootleg.world.Block
@@ -21,18 +22,22 @@ object CurrentBlockHUDRenderer {
   private val x3Block = blockScale * 3f
   private val x4Block = blockScale * 4f
   private val x10Block = blockScale * 10f
+
   private val layout = GlyphLayout()
   private val airItem = Item(Material.AIR, 0u, 0u)
 
   fun render(screenRenderer: ScreenRenderer, world: World?) {
-    val entity = world?.engine?.getEntitiesFor(selectedMaterialComponentFamily)?.firstOrNull() ?: return
-
-    val item = entity.selectedItem ?: airItem
+    try {
+      val entity = world?.engine?.getEntitiesFor(selectedMaterialComponentFamily)?.firstOrNull() ?: return
+      val item = entity.selectedItem ?: airItem
     val texture = item.material.textureRegion ?: KAssets.handTexture
-    with(screenRenderer) {
-      batch.draw(texture.textureRegion, Gdx.graphics.width - x4Block, Gdx.graphics.height - x3Block, x2Block, x2Block)
-      layout.setText(font, "${item.stock} / ${item.maxStock}", Color.WHITE, x10Block, Align.right, true)
-      font.draw(batch, layout, Gdx.graphics.width - x10Block - blockScale / 2, Gdx.graphics.height - x4Block)
+      with(screenRenderer) {
+        batch.draw(texture.textureRegion, Gdx.graphics.width - x4Block, Gdx.graphics.height - x3Block, x2Block, x2Block)
+        layout.setText(font, "${item.stock} / ${item.maxStock}", Color.WHITE, x10Block, Align.right, true)
+        font.draw(batch, layout, Gdx.graphics.width - x10Block - blockScale / 2, Gdx.graphics.height - x4Block)
+      }
+    } catch (e: NullPointerException) {
+      Main.logger().error("HUD failed to render due to ${e.javaClass::getSimpleName}: ${e.message}")
     }
   }
 }
