0505ded873067044bf9f0316aa0a8458348df8fa
Karl Henrik Elg <klink100@gmail.com>
2023-07-13 22:27:11 +0200

    Log entity id when out of bounds

--- core/src/main/kotlin/no/elg/infiniteBootleg/world/ecs/system/OutOfBoundsSystem.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/ecs/system/OutOfBoundsSystem.kt
@@ -6,7 +6,7 @@ import no.elg.infiniteBootleg.Main
 import no.elg.infiniteBootleg.util.stringifyCompactLoc
 import no.elg.infiniteBootleg.world.ecs.UPDATE_PRIORITY_EARLY
 import no.elg.infiniteBootleg.world.ecs.basicStandaloneEntityFamily
-import no.elg.infiniteBootleg.world.ecs.components.NamedComponent.Companion.nameOrNull
+import no.elg.infiniteBootleg.world.ecs.components.required.IdComponent.Companion.id
 import no.elg.infiniteBootleg.world.ecs.components.required.PositionComponent.Companion.compactBlockLoc
 import no.elg.infiniteBootleg.world.ecs.components.required.PositionComponent.Companion.compactChunkLoc
 import no.elg.infiniteBootleg.world.ecs.components.required.WorldComponent.Companion.world
@@ -15,7 +15,7 @@ object OutOfBoundsSystem : IteratingSystem(basicStandaloneEntityFamily, UPDATE_P
   override fun processEntity(entity: Entity, deltaTime: Float) {
     val world = entity.world
     if (!world.isChunkLoaded(entity.compactChunkLoc)) {
-      Main.logger().log("OutOfBoundsSystem", "Entity ${entity.nameOrNull} is out of bounds at ${stringifyCompactLoc(entity.compactBlockLoc)}")
+      Main.logger().log("OutOfBoundsSystem", "Entity ${entity.id} is out of bounds at ${stringifyCompactLoc(entity.compactBlockLoc)}")
       world.engine.removeEntity(entity)
     }
   }
