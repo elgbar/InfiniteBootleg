63c4269e853616a4683a14fe3aecbd3c6336c1fb
Karl Henrik Elg <klink100@gmail.com>
2023-08-17 20:55:00 +0200

    Save the uuid of the world with the entity

--- core/src/main/kotlin/no/elg/infiniteBootleg/world/ecs/components/required/WorldComponent.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/ecs/components/required/WorldComponent.kt
@@ -3,6 +3,7 @@ package no.elg.infiniteBootleg.world.ecs.components.required
 import com.badlogic.ashley.core.Entity
 import ktx.ashley.EngineEntity
 import ktx.ashley.propertyFor
+import no.elg.infiniteBootleg.main.Main
 import no.elg.infiniteBootleg.protobuf.EntityKt
 import no.elg.infiniteBootleg.protobuf.ProtoWorld
 import no.elg.infiniteBootleg.util.with
@@ -17,10 +18,11 @@ data class WorldComponent(val world: World) : EntitySavableComponent {
     val Entity.worldComponent by propertyFor(mapper)
 
     override fun EngineEntity.loadInternal(protoEntity: ProtoWorld.Entity, state: World): WorldComponent = with(WorldComponent(state))
-    override fun ProtoWorld.Entity.checkShouldLoad(): Boolean = true
+    override fun ProtoWorld.Entity.checkShouldLoad(state: () -> World): Boolean =
+      (worldUUID == state().uuid).also { if (!it) Main.logger().error("Loaded entity in wrong world! Expected $worldUUID, but got ${state().uuid}") }
   }
 
   override fun EntityKt.Dsl.save() {
-    // TODO Is there any point in this being a EntitySavableComponent?
+    this.worldUUID = world.uuid
   }
 }
