43e94ccb5dbaf319671ce0b50ce760243b6a98ac
Karl Henrik Elg <klink100@gmail.com>
2023-08-18 21:54:24 +0200

    Minor changes

--- core/src/main/kotlin/no/elg/infiniteBootleg/screens/hud/DebugWindow.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/screens/hud/DebugWindow.kt
@@ -8,10 +8,10 @@ import com.kotcrab.vis.ui.widget.spinner.FloatSpinnerModel
 import ktx.actors.isShown
 import ktx.actors.onChange
 import ktx.actors.onClick
+import ktx.scene2d.KTable
 import ktx.scene2d.Scene2dDsl
 import ktx.scene2d.actors
 import ktx.scene2d.horizontalGroup
-import ktx.scene2d.vis.KVisWindow
 import ktx.scene2d.vis.separator
 import ktx.scene2d.vis.spinner
 import ktx.scene2d.vis.visLabel
@@ -56,7 +56,7 @@ private fun updateAllValues() {
 }
 
 @Scene2dDsl
-private fun KVisWindow.toggleableDebugButton(
+private fun KTable.toggleableDebugButton(
   name: String,
   booleanGetter: () -> Boolean,
   onToggle: () -> Unit
@@ -82,7 +82,7 @@ private fun KVisWindow.toggleableDebugButton(
 }
 
 @Scene2dDsl
-private fun KVisWindow.floatSpinner(name: String, srcValueGetter: () -> Number, min: Number, max: Number, step: Number, decimals: Int, onChange: (Float) -> Unit) {
+private fun KTable.floatSpinner(name: String, srcValueGetter: () -> Number, min: Number, max: Number, step: Number, decimals: Int, onChange: (Float) -> Unit) {
   val model = FloatSpinnerModel(srcValueGetter().toString(), min.toString(), max.toString(), step.toString(), decimals)
   spinner(name, model) {
     it.fillX()
@@ -98,7 +98,7 @@ private fun KVisWindow.floatSpinner(name: String, srcValueGetter: () -> Number, 
 }
 
 @Scene2dDsl
-private fun KVisWindow.floatSlider(name: String, srcValueGetter: () -> Float, min: Float, max: Float, step: Float, onChange: (Float) -> Unit) {
+private fun KTable.floatSlider(name: String, srcValueGetter: () -> Float, min: Float, max: Float, step: Float, onChange: (Float) -> Unit) {
   horizontalGroup {
     it.fillX()
     space(5f)
@@ -127,14 +127,14 @@ fun Stage.addDebugOverlay(world: ClientWorld): DebugWindow {
       val cols = 5
 
       @Scene2dDsl
-      fun aRow(theRow: () -> Unit) {
+      fun section(theRow: KTable.() -> Unit) {
         theRow()
         row()
       }
 
       @Scene2dDsl
       fun aSeparator() {
-        aRow {
+        section {
           separator {
             it.fillX()
             it.colspan(cols)
@@ -143,14 +143,14 @@ fun Stage.addDebugOverlay(world: ClientWorld): DebugWindow {
       }
 
       //
-      aRow {
+      section {
         toggleableDebugButton("General debug", Settings::debug, Main.inst().console.exec::debug)
         toggleableDebugButton("Render chunks borders", Settings::renderChunkBounds, Main.inst().console.exec::debChu)
         toggleableDebugButton("Render chunk updates", Settings::renderChunkUpdates, Main.inst().console.exec::debChuUpd)
         toggleableDebugButton("Render entity markers", Settings::debugEntityMarkerBlocks, EntityMarkerBlock::toggleDebugEntityMarkerBlocks)
       }
       // Lights
-      aRow {
+      section {
         toggleableDebugButton("Render entity lighting", Settings::debugEntityLight, Main.inst().console.exec::debEntLit)
         toggleableDebugButton("Render block lighting", Settings::debugBlockLight, Main.inst().console.exec::debBlkLit)
         toggleableDebugButton("Render light updates", Settings::renderBlockLightUpdates, Main.inst().console.exec::debLitUpd)
@@ -158,8 +158,8 @@ fun Stage.addDebugOverlay(world: ClientWorld): DebugWindow {
       }
 
       aSeparator()
-      // Player related debug controls
-      aRow {
+      // Player related debug controlls
+      section {
         val entities = world.controlledPlayerEntities
         val instantBreakGetter: () -> Boolean = {
           entities
@@ -183,7 +183,7 @@ fun Stage.addDebugOverlay(world: ClientWorld): DebugWindow {
         floatSpinner("Reach radius", reachRadiusGetter, 1f, 512f, 1f, 0, Main.inst().console.exec::interactRadius)
       }
 
-      aRow {
+      section {
         val zoomValueGetter: () -> Float = { ClientMain.inst().world?.render?.camera?.zoom ?: 1f }
         floatSlider("Zoom", zoomValueGetter, 0.1f, MAX_ZOOM * 5, 0.1f) {
           val clientWorld = ClientMain.inst().world ?: return@floatSlider
@@ -193,21 +193,21 @@ fun Stage.addDebugOverlay(world: ClientWorld): DebugWindow {
       aSeparator()
 
       val box2dDebug = world.render.box2DDebugRenderer
-      aRow {
+      section {
         toggleableDebugButton("Debug Box2D", Settings::renderBox2dDebug, Main.inst().console.exec::debBox)
       }
-      aRow {
+      section {
         toggleableDebugButton("Box2D draw bodies", box2dDebug::isDrawBodies, Main.inst().console.exec::drawBodies)
         toggleableDebugButton("Box2D draw joints", box2dDebug::isDrawJoints, Main.inst().console.exec::drawJoints)
         toggleableDebugButton("Box2D draw AABBs", box2dDebug::isDrawAABBs, Main.inst().console.exec::drawAABBs)
         toggleableDebugButton("Box2D draw inactiveBodies", box2dDebug::isDrawInactiveBodies, Main.inst().console.exec::drawInactiveBodies)
       }
-      aRow {
+      section {
         toggleableDebugButton("Box2D draw velocities", box2dDebug::isDrawVelocities, Main.inst().console.exec::drawVelocities)
         toggleableDebugButton("Box2D draw contacts", box2dDebug::isDrawContacts, Main.inst().console.exec::drawContacts)
       }
       aSeparator()
-      aRow {
+      section {
         toggleableDebugButton("Vsync", Settings::vsync) { Settings.vsync = !Settings.vsync }
         floatSpinner("Max FPS", Settings::foregroundFPS, 0, 512, 1, 0) { Settings.foregroundFPS = it.toInt() }
         val oneDayInSeconds = 86400f
