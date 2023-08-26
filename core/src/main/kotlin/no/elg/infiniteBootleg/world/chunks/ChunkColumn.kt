10b73c379a9b9126774fac149d9ec8dfc093e899
Karl Henrik Elg <klink100@gmail.com>
2023-08-26 11:38:13 +0200

    Use K2 compiler

--- core/src/main/kotlin/no/elg/infiniteBootleg/world/chunks/ChunkColumn.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/chunks/ChunkColumn.kt
@@ -92,7 +92,7 @@ interface ChunkColumn {
       /**
        * The absolute top-most block, contains all flags
        */
-      const val TOP_MOST_FLAG: ChunkColumnFeatureFlag = BLOCKS_LIGHT_FLAG or SOLID_FLAG
+      const val TOP_MOST_FLAG: ChunkColumnFeatureFlag = 1 shl 1 or 1 shl 0 // BLOCKS_LIGHT_FLAG or SOLID_FLAG
     }
   }
 }
