22d096d11c8b2e352d01ef2b1118773f6d2857af
Karl Henrik Elg <klink100@gmail.com>
2023-10-14 21:43:04 +0200

    Make InitialChunksOfWorldLoadedEvent not async

--- core/src/main/kotlin/no/elg/infiniteBootleg/events/InitialChunksOfWorldLoadedEvent.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/events/InitialChunksOfWorldLoadedEvent.kt
@@ -1,10 +1,9 @@
 package no.elg.infiniteBootleg.events
 
-import no.elg.infiniteBootleg.events.api.AsyncEvent
-import no.elg.infiniteBootleg.events.api.ThreadType
+import no.elg.infiniteBootleg.events.api.Event
 import no.elg.infiniteBootleg.world.world.World
 
 /**
  * Event fired when all the initial chunks of the world is loaded.
  */
-data class InitialChunksOfWorldLoadedEvent(val world: World) : AsyncEvent(ThreadType.ASYNC, ThreadType.PHYSICS)
+data class InitialChunksOfWorldLoadedEvent(val world: World) : Event
