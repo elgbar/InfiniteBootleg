24684e7b42e72bc7091ea87d954d6569b93ac838
Karl Henrik Elg <klink100@gmail.com>
2023-11-20 17:17:28 +0100

    Cleanup server client chunk loader

--- core/src/main/kotlin/no/elg/infiniteBootleg/world/loader/chunk/ServerClientChunkLoader.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/loader/chunk/ServerClientChunkLoader.kt
@@ -2,39 +2,31 @@ package no.elg.infiniteBootleg.world.loader.chunk
 
 import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
-import no.elg.infiniteBootleg.protobuf.ProtoWorld
 import no.elg.infiniteBootleg.server.serverBoundChunkRequestPacket
 import no.elg.infiniteBootleg.util.ChunkCompactLoc
 import no.elg.infiniteBootleg.util.component1
 import no.elg.infiniteBootleg.util.component2
-import no.elg.infiniteBootleg.util.toCompact
-import no.elg.infiniteBootleg.world.chunks.Chunk
 import no.elg.infiniteBootleg.world.generator.chunk.ChunkGenerator
 import no.elg.infiniteBootleg.world.world.ServerClientWorld
 import java.time.Duration
 
 class ServerClientChunkLoader(override val world: ServerClientWorld, generator: ChunkGenerator) : ChunkLoader(generator) {
 
-  private val toBeLoadedChunks: Cache<ChunkCompactLoc, Boolean> =
+  private val toBeLoadedChunks: Cache<ChunkCompactLoc, LoadedChunk> =
     Caffeine.newBuilder()
       .expireAfterWrite(Duration.ofSeconds(1))
       .build()
 
-  private val loadedChunk = LoadedChunk(null, false)
-
-  override fun fetchChunk(chunkLoc: ChunkCompactLoc): LoadedChunk {
-    toBeLoadedChunks.getIfPresent(chunkLoc)
+  override fun fetchChunk(chunkLoc: ChunkCompactLoc): LoadedChunk =
     toBeLoadedChunks.get(chunkLoc) {
+      //Abuse cache to only fetch chunk if we haven't done so within the last second
       val (chunkX, chunkY) = chunkLoc
       val serverClient = world.serverClient
       serverClient.ctx.writeAndFlush(serverClient.serverBoundChunkRequestPacket(chunkX, chunkY))
-      true
+      NULL_LOADED_CHUNK
     }
-    return loadedChunk
-  }
 
-  override fun loadChunkFromProto(protoChunk: ProtoWorld.Chunk): Chunk? {
-    toBeLoadedChunks.invalidate(protoChunk.position.toCompact())
-    return super.loadChunkFromProto(protoChunk)
+  companion object {
+    private val NULL_LOADED_CHUNK = LoadedChunk(null, false)
   }
 }
