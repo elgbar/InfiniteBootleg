6c11123f49dc51a174cc36808405a73c618b08a6
Karl Henrik Elg <klink100@gmail.com>
2023-12-31 14:05:33 +0100

    Name magic constants

--- core/src/main/kotlin/no/elg/infiniteBootleg/util/NoiseUtils.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/util/NoiseUtils.kt
@@ -3,11 +3,17 @@ package no.elg.infiniteBootleg.util
 import no.elg.infiniteBootleg.world.generator.noise.FastNoiseLite
 
 /**
- * @return Noise output bounded between 0..1
+ * @return Noise output bounded between 0..[amplitude]
  */
-fun FastNoiseLite.getNoise(x: Number, y: Number): Float = (1 + GetNoise(x.toDouble(), y.toDouble())) / 2f
+fun FastNoiseLite.getNoise(x: Number, y: Number, amplitude: Float = 1f): Float {
+  val randomNumber = (1.0 + GetNoise(x.toDouble(), y.toDouble())) / 2.0
+  return (randomNumber * amplitude.toDouble()).toFloat()
+}
 
 /**
  * @return Noise output bounded between 0..1
  */
-fun FastNoiseLite.getNoise(x: Number, y: Number, z: Number): Float = (1 + GetNoise(x.toDouble(), y.toDouble(), z.toDouble())) / 2f
+fun FastNoiseLite.getNoise(x: Number, y: Number, z: Number, amplitude: Float = 1f): Float {
+  val randomNumber = (1 + GetNoise(x.toDouble(), y.toDouble(), z.toDouble())) / 2f
+  return (randomNumber * amplitude.toDouble()).toFloat()
+}
--- core/src/main/kotlin/no/elg/infiniteBootleg/world/generator/features/ForestGenerator.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/generator/features/ForestGenerator.kt
@@ -6,6 +6,7 @@ import no.elg.infiniteBootleg.world.Material
 import no.elg.infiniteBootleg.world.chunks.Chunk
 import no.elg.infiniteBootleg.world.generator.noise.FastNoiseLite
 import no.elg.infiniteBootleg.world.world.World
+import no.elg.infiniteBootleg.world.world.World.Companion.HALF_BLOCK_SIZE
 import no.elg.infiniteBootleg.world.world.World.Companion.getLocationsWithin
 import kotlin.math.roundToInt
 
@@ -24,15 +25,15 @@ class ForestGenerator(override val seed: Long, private val cutoff: Float) : Feat
   }
 
   private fun createTree(world: World, worldX: WorldCoord, worldY: WorldCoord) {
-    val treeHeight = (shouldGenerateTreeNoise.getNoise(worldX, worldY, 1) * 16).coerceAtLeast(3f).roundToInt()
+    val treeHeight = shouldGenerateTreeNoise.getNoise(worldX, worldY, 1, MAX_TREE_HEIGHT).coerceAtLeast(MIN_TREE_HEIGHT).roundToInt()
     val locs = World.getLocationsAABBFromCorner(worldX.toFloat(), worldY.toFloat() + treeHeight, 0f, treeHeight.toFloat())
     Material.BIRCH_TRUNK.createBlocks(world, locs, prioritize = false, allowOverwiteNonAir = true)
 
-    val leavesRadius = (shouldGenerateTreeNoise.getNoise(worldX, worldY, 2) * 5).coerceAtLeast(3f)
-    generateLeavesBlob(world, worldX, worldY + treeHeight.toInt(), leavesRadius)
+    val leavesRadius = shouldGenerateTreeNoise.getNoise(worldX, worldY, 2, MAX_PRIMARY_LEAVES_RADIUS).coerceAtLeast(MIN_PRIMARY_LEAVES_RADIUS)
+    generateLeavesBlob(world, worldX, worldY + treeHeight, leavesRadius)
 
-    if (treeHeight >= 6) {
-      val leavesRadius2 = (shouldGenerateTreeNoise.getNoise(worldX, worldY, 3) * 4).coerceIn(1f, leavesRadius - 0.5f)
+    if (treeHeight >= MIN_TREE_HEIGHT_FOR_SECONDARY_LEAVES) {
+      val leavesRadius2 = shouldGenerateTreeNoise.getNoise(worldX, worldY, 3, MAX_SECONDARY_LEAVES_RADIUS).coerceIn(MIN_SECONDARY_LEAVES_RADIUS, leavesRadius - HALF_BLOCK_SIZE)
       generateLeavesBlob(world, worldX, worldY + (treeHeight - leavesRadius).toInt(), leavesRadius2)
     }
   }
@@ -40,5 +41,18 @@ class ForestGenerator(override val seed: Long, private val cutoff: Float) : Feat
   private fun generateLeavesBlob(world: World, worldX: WorldCoord, worldY: WorldCoord, radius: Float) {
     val leaves = getLocationsWithin(worldX, worldY, radius)
     Material.BIRCH_LEAVES.createBlocks(world, leaves, false)
+  }
+
+  companion object {
+    const val MAX_TREE_HEIGHT = 16f
+    const val MIN_TREE_HEIGHT = 3f
+
+    const val MAX_PRIMARY_LEAVES_RADIUS = 5f
+    const val MIN_PRIMARY_LEAVES_RADIUS = 3f
+
+    const val MIN_TREE_HEIGHT_FOR_SECONDARY_LEAVES = 6
+
+    const val MAX_SECONDARY_LEAVES_RADIUS = 4f
+    const val MIN_SECONDARY_LEAVES_RADIUS = 1f
   }
 }
