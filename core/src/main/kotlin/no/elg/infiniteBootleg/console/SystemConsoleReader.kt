682a0d776ecad7d42e3a49a23bd31be9e3f9a867
Karl Henrik Elg <klink100@gmail.com>
2024-06-14 21:25:25 +0200

    Fix system console never being used

--- core/src/main/kotlin/no/elg/infiniteBootleg/console/SystemConsoleReader.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/console/SystemConsoleReader.kt
@@ -2,6 +2,7 @@ package no.elg.infiniteBootleg.console
 
 import com.badlogic.gdx.utils.Disposable
 import no.elg.infiniteBootleg.main.Main
+import java.io.Console
 import java.util.Scanner
 
 /** Read input from [System.console] or [System. in] if no console exists.  */
@@ -18,19 +19,15 @@ class SystemConsoleReader(private val consoleHandler: InGameConsoleHandler) : Ru
       return
     }
     running = true
-    val console = System.console()
-    if (console != null) {
-      Scanner(console.reader())
-    } else {
-      Scanner(System.`in`).use { scanner ->
-        while (running) {
-          var read: String?
-          try {
-            read = scanner.nextLine()
-            Main.inst().scheduler.executeSync { consoleHandler.execCommand(read) }
-          } catch (e: Exception) {
-            dispose()
-          }
+    val scanner = System.console()?.run(Console::reader)?.let(::Scanner) ?: Scanner(System.`in`)
+    scanner.use { openScanner ->
+      while (running) {
+        var read: String?
+        try {
+          read = openScanner.nextLine()
+          Main.inst().scheduler.executeSync { consoleHandler.execCommand(read) }
+        } catch (e: Exception) {
+          dispose()
         }
       }
     }
