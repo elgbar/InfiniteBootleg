685c65b389ff8fbeb86b245c04201da363b07ac3
Karl Henrik Elg <klink100@gmail.com>
2024-06-14 17:11:38 +0200

    Do not show logger name

--- core/src/main/kotlin/no/elg/infiniteBootleg/logging/InGameConsoleAppender.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/logging/InGameConsoleAppender.kt
@@ -16,17 +16,27 @@ import org.apache.logging.log4j.core.config.plugins.PluginAttribute
 import org.apache.logging.log4j.core.config.plugins.PluginFactory
 
 @Plugin(name = "InGameConsole", category = Core.CATEGORY_NAME, elementType = Appender.ELEMENT_TYPE, printObject = true)
-class InGameConsoleAppender(name: String) : AbstractAppender(name, null, null, true, null) {
+class InGameConsoleAppender(name: String, val excludeLoggerName: Boolean) : AbstractAppender(name, null, null, true, null) {
 
   override fun append(event: LogEvent) {
     val console = CommonMain.instField?.console ?: return
     if (console.inGameConsole) {
-      when (event.level) {
-        ERROR, FATAL -> console.error(event.loggerName, event.message.toString())
-        WARN -> console.warn(event.loggerName, event.message.toString())
-        INFO -> console.log(event.loggerName, event.message.toString())
-        DEBUG -> console.debug(event.loggerName, event.message.toString())
-        TRACE -> console.debug("TRACE ${event.loggerName}", event.message.toString())
+      if (excludeLoggerName) {
+        when (event.level) {
+          ERROR, FATAL -> console.error(event.message.toString())
+          WARN -> console.warn(event.message.toString())
+          INFO -> console.log(event.message.toString())
+          DEBUG -> console.log("DEBUG", event.message.toString())
+          TRACE -> console.log("TRACE", event.message.toString())
+        }
+      } else {
+        when (event.level) {
+          ERROR, FATAL -> console.error(event.loggerName, event.message.toString())
+          WARN -> console.warn(event.loggerName, event.message.toString())
+          INFO -> console.log(event.loggerName, event.message.toString())
+          DEBUG -> console.debug(event.loggerName, event.message.toString())
+          TRACE -> console.debug("TRACE ${event.loggerName}", event.message.toString())
+        }
       }
     }
   }
@@ -35,6 +45,7 @@ class InGameConsoleAppender(name: String) : AbstractAppender(name, null, null, t
 
     @JvmStatic
     @PluginFactory
-    fun createAppender(@PluginAttribute("name") name: String): InGameConsoleAppender = InGameConsoleAppender(name)
+    fun createAppender(@PluginAttribute("name") name: String, @PluginAttribute("excludeLoggerName") excludeLoggerName: Boolean): InGameConsoleAppender =
+      InGameConsoleAppender(name, excludeLoggerName)
   }
 }
