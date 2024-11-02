c8cdb52e57666873ada7fdf2ce0eb0e13cbf47d5
Karl Henrik Elg <klink100@gmail.com>
2024-11-02 11:46:41 +0100

    Do not write lock when tick world

--- core/src/main/kotlin/no/elg/infiniteBootleg/world/ticker/WorldTickee.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/ticker/WorldTickee.kt
@@ -10,14 +10,13 @@ import no.elg.infiniteBootleg.util.launchOnAsync
 import no.elg.infiniteBootleg.util.stringifyCompactLoc
 import no.elg.infiniteBootleg.world.chunks.Chunk
 import no.elg.infiniteBootleg.world.world.World
-import kotlin.concurrent.read
 
 private val logger = KotlinLogging.logger {}
 
 internal class WorldTickee(private val world: World) : Ticking {
 
   @GuardedBy("world.chunksLock")
-  private val chunkIterator: LongMap.Entries<Chunk> = LongMap.Entries(world.chunks)
+  private val chunkIterator: LongMap.Entries<Chunk> = world.createChunkIterator()
   private val worldTickedEvent = WorldTickedEvent(world)
 
   @Synchronized
@@ -27,7 +26,7 @@ internal class WorldTickee(private val world: World) : Ticking {
     // tick all chunks and blocks in chunks
     val tick = world.worldTicker.tickId
 
-    world.chunksLock.read {
+    world.readChunks {
       chunkIterator.reset()
       while (chunkIterator.hasNext()) {
         val chunk: Chunk? = chunkIterator.next().value
@@ -35,15 +34,12 @@ internal class WorldTickee(private val world: World) : Ticking {
         // clean up dead chunks
         if (chunk == null) {
           logger.warn { "Found null chunk when ticking world" }
-          chunkIterator.remove()
           continue
         } else if (chunk.isDisposed) {
           logger.warn { "Found disposed chunk ${stringifyCompactLoc(chunk.chunkX, chunk.chunkY)} when ticking world" }
-          chunkIterator.remove()
           continue
         }
         if (chunk.isAllowedToUnload && world.render.isOutOfView(chunk) && tick - chunk.lastViewedTick > chunkUnloadTime) {
-          chunkIterator.remove()
           launchOnAsync {
             world.unloadChunk(chunk, force = false, save = true)
           }
