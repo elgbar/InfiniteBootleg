8e29b3ecd6b7fa2885981731dd4c6d86ac5790c9
Karl Henrik Elg <klink100@gmail.com>
2024-11-09 11:24:00 +0100

    Fix falling block looking to infinity

--- core/src/main/kotlin/no/elg/infiniteBootleg/world/box2d/service/FallingBlockContactService.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/world/box2d/service/FallingBlockContactService.kt
@@ -1,19 +1,25 @@
 package no.elg.infiniteBootleg.world.box2d.service
 
 import com.badlogic.ashley.core.Entity
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.elg.infiniteBootleg.main.Main
 import no.elg.infiniteBootleg.protobuf.Packets
+import no.elg.infiniteBootleg.util.isAir
 import no.elg.infiniteBootleg.util.isBeingRemoved
-import no.elg.infiniteBootleg.util.isMarkerBlock
-import no.elg.infiniteBootleg.world.Material
+import no.elg.infiniteBootleg.util.isNotAir
 import no.elg.infiniteBootleg.world.blocks.Block
 import no.elg.infiniteBootleg.world.blocks.Block.Companion.worldX
 import no.elg.infiniteBootleg.world.blocks.Block.Companion.worldY
+import no.elg.infiniteBootleg.world.chunks.Chunk
 import no.elg.infiniteBootleg.world.ecs.components.MaterialComponent.Companion.materialOrNull
 import no.elg.infiniteBootleg.world.ecs.components.events.PhysicsEvent
 import no.elg.infiniteBootleg.world.ecs.system.event.PhysicsSystem.getOtherFixtureUserData
 
 object FallingBlockContactService {
+
+  private val logger = KotlinLogging.logger {}
+
+  private const val MAX_DELTA_UP = Chunk.CHUNK_SIZE
 
   fun handleFallingBlockContactBeginsEvent(entity: Entity, event: PhysicsEvent.ContactBeginsEvent) {
     if (Main.isAuthoritative) {
@@ -31,12 +37,16 @@ object FallingBlockContactService {
         var deltaY = 0
         var blockUp: Block?
         do {
+          if (deltaY > MAX_DELTA_UP) {
+            logger.trace { "Reached max delta up for handling falling block contact" }
+            return
+          }
           deltaY++
-          blockUp = world.getBlock(newX, newY + deltaY, false)
-        } while (blockUp?.material == material && !blockUp.isMarkerBlock())
+          blockUp = world.getBlock(newX, newY + deltaY, true)
+        } while (blockUp.isNotAir(true))
 
         // Only place the block if there is actual free space above
-        if (blockUp?.material == Material.AIR || blockUp.isMarkerBlock()) {
+        if (blockUp.isAir(true)) {
           world.setBlock(newX, newY + deltaY, material)
         }
       }
