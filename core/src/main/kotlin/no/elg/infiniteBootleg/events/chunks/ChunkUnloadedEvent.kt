4a414d418810f45d25c612e2cc11e6fe40c55415
Karl Henrik Elg <klink100@gmail.com>
2024-12-30 21:59:00 +0100

    ChunkUnloadedEvent not async

--- core/src/main/kotlin/no/elg/infiniteBootleg/events/chunks/ChunkUnloadedEvent.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/events/chunks/ChunkUnloadedEvent.kt
@@ -1,15 +1,12 @@
 package no.elg.infiniteBootleg.events.chunks
 
-import no.elg.infiniteBootleg.events.api.AsyncEvent
 import no.elg.infiniteBootleg.events.api.ReasonedEvent
-import no.elg.infiniteBootleg.events.api.ThreadType
 import no.elg.infiniteBootleg.world.chunks.Chunk
 
 /**
  * Fired when a chunk is about to be unloaded. It will still be valid while this event is dispatched
  */
-data class ChunkUnloadedEvent(override val chunk: Chunk) : ChunkEvent, ReasonedEvent,
-  AsyncEvent(ThreadType.TICKER, ThreadType.ASYNC, ThreadType.PHYSICS) {
+data class ChunkUnloadedEvent(override val chunk: Chunk) : ChunkEvent, ReasonedEvent {
   override val reason: String
     get() = "Chunk unloaded"
 }
