86decadcf9f5cf8fd4734d74516d3473956a81f6
Karl Henrik Elg <klink100@gmail.com>
2025-02-05 20:12:52 +0100

    Use standardized send serverbound packet

--- client/src/main/kotlin/no/elg/infiniteBootleg/client/net/ClientPacketSender.kt
+++ client/src/main/kotlin/no/elg/infiniteBootleg/client/net/ClientPacketSender.kt
@@ -1,26 +1,27 @@
 package no.elg.infiniteBootleg.client.net
 
 import no.elg.infiniteBootleg.client.main.ClientMain
-import no.elg.infiniteBootleg.core.main.Main
 import no.elg.infiniteBootleg.core.net.PacketSender
 import no.elg.infiniteBootleg.core.net.ServerClient
+import no.elg.infiniteBootleg.core.net.ServerClient.Companion.sendServerBoundPacket
 import no.elg.infiniteBootleg.core.util.ChunkCompactLoc
 import no.elg.infiniteBootleg.protobuf.Packets
 
 object ClientPacketSender : PacketSender {
 
+  @Deprecated(
+    "Use serverClient directly instead in the client module",
+    ReplaceWith("ClientMain.inst().serverClient.sendServerBoundPacket(ifIsClient)", "no.elg.infiniteBootleg.client.main.ClientMain")
+  )
   override fun sendDuplexPacket(ifIsServer: () -> Packets.Packet, ifIsClient: ServerClient.() -> Packets.Packet) {
-    if (Main.Companion.isServerClient) {
-      val client = ClientMain.inst().serverClient ?: error("Server client null after check")
-      client.ctx.writeAndFlushPacket(client.ifIsClient())
-    }
+    ClientMain.inst().serverClient.sendServerBoundPacket(ifIsClient)
   }
 
+  @Deprecated(
+    "Use serverClient directly instead in the client module",
+    ReplaceWith("ClientMain.inst().serverClient.sendServerBoundPacket(ifIsClient)", "no.elg.infiniteBootleg.client.main.ClientMain")
+  )
   override fun sendDuplexPacketInView(ifIsServer: () -> Pair<Packets.Packet?, ChunkCompactLoc>, ifIsClient: ServerClient.() -> Packets.Packet?) {
-    if (Main.Companion.isServerClient) {
-      val client = ClientMain.inst().serverClient ?: error("Server client null after check")
-      val packet = client.ifIsClient() ?: return
-      client.ctx.writeAndFlushPacket(packet)
-    }
+    ClientMain.inst().serverClient.sendServerBoundPacket(ifIsClient)
   }
 }
