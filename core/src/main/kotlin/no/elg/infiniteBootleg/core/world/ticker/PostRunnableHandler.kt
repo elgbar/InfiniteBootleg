d63eaa9f7b7417a1f16ee25b03c71cf467e71a1f
Karl Henrik Elg <klink100@gmail.com>
2025-08-23 15:11:12 +0200

    Fix async debugging

--- core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ticker/PostRunnableHandler.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ticker/PostRunnableHandler.kt
@@ -3,14 +3,15 @@ package no.elg.infiniteBootleg.core.world.ticker
 import com.badlogic.gdx.utils.Array.ArrayIterator
 import ktx.collections.GdxArray
 import no.elg.infiniteBootleg.core.util.IllegalAction
+import org.jetbrains.annotations.Async
 
 class PostRunnableHandler : PostRunnable {
 
-  private val runnables = GdxArray<Runnable>()
-  private val executedRunnables = GdxArray<Runnable>()
+  private val runnables = GdxArray<() -> Unit>()
+  private val executedRunnables = GdxArray<() -> Unit>()
   private val executedRunnablesIterator = ArrayIterator(executedRunnables)
 
-  override fun postRunnable(runnable: () -> Unit) {
+  override fun postRunnable(@Async.Schedule runnable: () -> Unit) {
     synchronized(runnables) {
       runnables.add(runnable)
     }
@@ -30,11 +31,15 @@ class PostRunnableHandler : PostRunnable {
     }
     executedRunnablesIterator.reset()
     for (runnable in executedRunnablesIterator) {
-      try {
-        runnable.run()
-      } catch (e: Exception) {
-        IllegalAction.LOG.handle(e) { "Failed to run task" }
-      }
+      execute(runnable)
+    }
+  }
+
+  fun execute(@Async.Execute runnable: () -> Unit) {
+    try {
+      runnable()
+    } catch (e: Exception) {
+      IllegalAction.LOG.handle(e) { "Failed to run task" }
     }
   }
 }
