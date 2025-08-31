7265c36ec1a7e2c9c6ad1f5a3f1099114eaf9079
Karl Henrik Elg <klink100@gmail.com>
2025-09-01 00:08:11 +0200

    Wrong import

--- core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ecs/system/event/PhysicsSystem.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ecs/system/event/PhysicsSystem.kt
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Entity
 import no.elg.infiniteBootleg.core.main.Main
 import no.elg.infiniteBootleg.core.world.box2d.service.AuthoritativeFallingBlockPhysicsEventHandler
 import no.elg.infiniteBootleg.core.world.box2d.service.OnGroundPhysicsEventHandler
+import no.elg.infiniteBootleg.core.world.box2d.service.SpellContactPhysicsEventHandler
 import no.elg.infiniteBootleg.core.world.ecs.components.PhysicsEventQueueComponent
 import no.elg.infiniteBootleg.core.world.ecs.components.events.PhysicsEvent
 import no.elg.infiniteBootleg.core.world.ecs.entityWithPhysicsEventFamily
