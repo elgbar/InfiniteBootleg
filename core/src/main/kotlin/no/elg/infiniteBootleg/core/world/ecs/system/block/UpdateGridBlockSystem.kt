7b3a1000a1c22a392ddd077ad045350a0d67a0ca
Karl Henrik Elg <klink100@gmail.com>
2025-10-25 11:32:31 +0200

    Optimize update grip block system

--- core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ecs/system/block/UpdateGridBlockSystem.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ecs/system/block/UpdateGridBlockSystem.kt
@@ -3,6 +3,8 @@ package no.elg.infiniteBootleg.core.world.ecs.system.block
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IteratingSystem
 import io.github.oshai.kotlinlogging.KotlinLogging
+import ktx.ashley.has
+import ktx.collections.isNotEmpty
 import ktx.collections.plusAssign
 import ktx.collections.removeAll
 import no.elg.infiniteBootleg.core.util.stringifyCompactLocWithChunk
@@ -11,7 +13,8 @@ import no.elg.infiniteBootleg.core.world.blocks.Block.Companion.validChunkOrLoad
 import no.elg.infiniteBootleg.core.world.blocks.EntityMarkerBlock
 import no.elg.infiniteBootleg.core.world.ecs.UPDATE_PRIORITY_DEFAULT
 import no.elg.infiniteBootleg.core.world.ecs.components.Box2DBodyComponent.Companion.box2d
-import no.elg.infiniteBootleg.core.world.ecs.components.OccupyingBlocksComponent.Companion.occupyingLocations
+import no.elg.infiniteBootleg.core.world.ecs.components.OccupyingBlocksComponent.Companion.occupyingBlocksComponent
+import no.elg.infiniteBootleg.core.world.ecs.components.VelocityComponent
 import no.elg.infiniteBootleg.core.world.ecs.components.required.PositionComponent.Companion.positionComponent
 import no.elg.infiniteBootleg.core.world.ecs.components.required.WorldComponent.Companion.world
 import no.elg.infiniteBootleg.core.world.ecs.standaloneGridOccupyingBlocksFamily
@@ -27,8 +30,12 @@ object UpdateGridBlockSystem : IteratingSystem(standaloneGridOccupyingBlocksFami
     val world = entity.world
     val pos = entity.positionComponent
     val box2d = entity.box2d
-    val halfBox2dWidth = box2d.halfBox2dWidth
-    val halfBox2dHeight = box2d.halfBox2dHeight
+    val occupyingBlocksComponent = entity.occupyingBlocksComponent
+
+    if (!entity.has(VelocityComponent.mapper) && occupyingBlocksComponent.occupying.isNotEmpty()) {
+      // The entity will not move, so only update it only once
+      return
+    }
 
     // Note: raw must be false to properly update the lights while lights are falling
     // Note 2: loadChunk must be false as the entity should then be handled as out of bounds
@@ -36,11 +43,11 @@ object UpdateGridBlockSystem : IteratingSystem(standaloneGridOccupyingBlocksFami
       world.getBlocksAABB(pos.blockX.toFloat(), pos.blockY.toFloat(), box2d.box2dWidth - 1f, box2d.box2dHeight - 1f, raw = false, loadChunk = false, includeAir = true)
 
     // Remove markers that are no longer occupied
-    val noLongerOccupied = entity.occupyingLocations.filter { it !in currentOccupations }
-    entity.occupyingLocations.removeAll(noLongerOccupied)
+    val noLongerOccupied = occupyingBlocksComponent.occupying.filter { it !in currentOccupations }
+    occupyingBlocksComponent.occupying.removeAll(noLongerOccupied)
 
-    val newOccupation = currentOccupations.filter { it !in entity.occupyingLocations }
-    for (newOccupation in newOccupation) {
+    val newOccupations = currentOccupations.filter { it !in occupyingBlocksComponent.occupying }
+    for (newOccupation in newOccupations) {
       val validChunk = newOccupation.validChunkOrLoad ?: run {
         logger.error { "Failed to get valid chunk for block ${stringifyCompactLocWithChunk(newOccupation)}" }
         continue
@@ -50,7 +57,7 @@ object UpdateGridBlockSystem : IteratingSystem(standaloneGridOccupyingBlocksFami
           logger.error { "Failed to replace marker block ${stringifyCompactLocWithChunk(newOccupation)}" }
           continue
         }
-        entity.occupyingLocations += occupiedBlock
+        occupyingBlocksComponent.occupying += occupiedBlock
       }
     }
     // Remove markers after setting new markers to avoid a gap where there is no marker at all
