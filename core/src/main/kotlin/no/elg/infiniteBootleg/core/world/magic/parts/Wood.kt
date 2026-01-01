24231ff3074d926e673a432e230d844234d1e71a
Karl Henrik Elg <klink100@gmail.com>
2026-01-01 14:18:29 +0100

    Add hanging birch wood type

--- core/src/main/kotlin/no/elg/infiniteBootleg/core/world/magic/parts/Wood.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/core/world/magic/parts/Wood.kt
@@ -22,46 +22,50 @@ sealed class WoodType(val gemSlots: UInt, val ringSlots: UInt, val dryingRate: D
   Description {
 
   companion object {
+    val woodTypes: List<WoodType> by lazy { sealedSubclassObjectInstances<WoodType>() }
 
     fun valueOf(displayName: String): WoodType =
-      when (displayName) {
-        Birch.serializedName -> Birch
-        Aerowode.serializedName -> Aerowode
-        RedWood.serializedName -> RedWood
-        Driftwood.serializedName -> Driftwood
-        WistedWood.serializedName -> WistedWood
-        Trekant.serializedName -> Trekant
-        else -> throw IllegalArgumentException("Unknown wood type $displayName")
+      requireNotNull(woodTypes.find { it.serializedName == displayName }) {
+        "Unknown wood type $displayName"
       }
   }
 }
 
 data object Birch : WoodType(1u, 0u, 1.0, 333.milliseconds) {
-  override val description: String get() = "A common wood type known for its light color and smooth texture."
+  override val description: String get() = "A weakly magical, but common, wood type known for its light color."
   override val displayName: String get() = "Birch"
 }
 
-data object Aerowode : WoodType(1u, 2u, 1.0, 250.milliseconds) {
-  override val description: String get() = "A rare and lightweight wood that seems to almost float in the air."
+data object HangingBirch : WoodType(1u, 1u, 1.0, 333.milliseconds) {
+  override val description: String get() = "A slightly more magical wood to the common birch"
+  override val displayName: String get() = "Hanging Birch"
+}
+
+//TODO on equip: player's gravity is reduced by 20% * rating.powerPercent
+//TODO Imcompatible with `Lead` ring
+data object Aerowode : WoodType(1u, 0u, 1.0, 250.milliseconds) {
+  override val description: String get() = "A rare and lightweight wood that seems to almost float in the air"
   override val displayName: String get() = "Aerowode"
 }
 
+// TODO make it start random fires
 data object RedWood : WoodType(1u, 0u, 2.0, 150.milliseconds) {
-  override val description: String get() = "A sturdy and dense wood with a deep red hue. Dries faster than most woods."
+  override val description: String get() = "The wood crackles and smokes, making it dry very quickly"
   override val displayName: String get() = "Redwood"
 }
 
+// todo make it work under water
 data object Driftwood : WoodType(1u, 3u, 0.25, 300.milliseconds) {
-  override val description: String get() = "A wood found washed up on shores, weathered by salt and time, and so hard to dry."
+  override val description: String get() = "Even when dried, drops of water forms around its base"
   override val displayName: String get() = "Driftwood"
 }
 
 data object WistedWood : WoodType(2u, 1u, 1.0, 100.milliseconds) {
-  override val description: String get() = "A twisted and gnarled wood, known for its strength and resilience."
+  override val description: String get() = "A magical twisted and gnarled wood, known for its ring capacity and speed"
   override val displayName: String get() = "Wistedwood"
 }
 
 data object Trekant : WoodType(3u, 2u, 1.0, 50.milliseconds) {
-  override val description: String get() = "A triangular cross-section wood, prized for its unique shape and magical properties."
+  override val description: String get() = "A legendary triangular cross-section wood, prized for its unique shape and magical properties."
   override val displayName: String get() = "Trekant"
 }
