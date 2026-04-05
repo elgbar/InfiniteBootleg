1f687ae518c9238dd9e46abb32dff9add1d88c63
Karl Henrik Elg <klink100@gmail.com>
2026-04-05 12:58:37 +0200

    Actually add the material categories

---
+++ core/src/main/kotlin/no/elg/infiniteBootleg/core/world/MaterialCategory.kt
@@ -0,0 +1,12 @@
+package no.elg.infiniteBootleg.core.world
+
+/**
+ * Categories of materials useful to group similar [[Material]] together
+ */
+enum class MaterialCategory {
+  SOIL,
+  PLAIN_ROCK,
+  ORE,
+  ORGANIC,
+  CRAFTED
+}
