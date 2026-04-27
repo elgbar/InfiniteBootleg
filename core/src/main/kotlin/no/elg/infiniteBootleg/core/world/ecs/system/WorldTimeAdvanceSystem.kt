eebca7d32eeef150c8bfe8bc1099d200e7fe9321
Karl Henrik Elg <klink100@gmail.com>
2026-04-27 20:32:55 +0200

    Simplify WorldTimeAdvanceSystem

--- core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ecs/system/WorldTimeAdvanceSystem.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/core/world/ecs/system/WorldTimeAdvanceSystem.kt
@@ -1,17 +1,12 @@
 package no.elg.infiniteBootleg.core.world.ecs.system
 
-import com.badlogic.ashley.systems.IntervalSystem
-import no.elg.infiniteBootleg.core.world.ecs.UPDATE_PRIORITY_LATE
+import com.badlogic.ashley.core.EntitySystem
+import no.elg.infiniteBootleg.core.world.ecs.UPDATE_PRIORITY_EARLY
 import no.elg.infiniteBootleg.core.world.world.World
 
-class WorldTimeAdvanceSystem(private val world: World) : IntervalSystem(WORLD_TIME_INTERVAL_SECONDS, UPDATE_PRIORITY_LATE) {
-
-  override fun updateInterval() {
+class WorldTimeAdvanceSystem(private val world: World) : EntitySystem(UPDATE_PRIORITY_EARLY) {
+  override fun update(deltaTime: Float) {
     val time = world.worldTime
-    time.time += time.timeScale * 1 / 30f
-  }
-
-  companion object {
-    private const val WORLD_TIME_INTERVAL_SECONDS = 1f
+    time.time += time.timeScale * deltaTime
   }
 }
