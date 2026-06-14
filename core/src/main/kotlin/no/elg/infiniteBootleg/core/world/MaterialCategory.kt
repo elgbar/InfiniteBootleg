d6234810e64f26eb88dc16abd3d95acaca37bb6d
Karl Henrik Elg <klink100@gmail.com>
2026-06-14 18:00:51 +0200

    Document material categories

--- core/src/main/kotlin/no/elg/infiniteBootleg/core/world/MaterialCategory.kt
+++ core/src/main/kotlin/no/elg/infiniteBootleg/core/world/MaterialCategory.kt
@@ -1,13 +1,36 @@
 package no.elg.infiniteBootleg.core.world
 
 /**
- * Categories of materials useful to group similar [[Material]] together
+ * Categories of materials useful to group similar [Material] together
  */
 enum class MaterialCategory {
+  /**
+   * Loose natural occurring material
+   */
   SOIL,
+
+  /**
+   * Hard natural occurring material
+   */
   PLAIN_ROCK,
+
+  /**
+   * Natural material containing a resource
+   */
   ORE,
+
+  /**
+   * Living stuff, which are also material
+   */
   ORGANIC,
+
+  /**
+   * Man-made material
+   */
   CRAFTED,
+
+  /**
+   * Magically created material
+   */
   MAGIC
 }
