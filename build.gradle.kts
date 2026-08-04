e4c7b0554c5e2429eb00d6e3dbc312ce4ae2a698
Karl Henrik Elg <klink100@gmail.com>
2026-08-04 19:19:28 +0200

    Replace ktlint with ktfmt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.diffplug.spotless.kotlin.KtfmtStep
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import com.google.protobuf.gradle.GenerateProtoTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
@@ -57,15 +58,13 @@ allprojects {
     kotlin {
       target("**/*.gradle.kts", "**/*.kt")
       targetExclude(fileTree(projectDir) { include("**/generated/**") })
-      ktlint().setEditorConfigPath("$rootDir/.editorconfig").editorConfigOverride(
-        mapOf(
-          "ktlint_function_signature_rule_force_multiline_when_parameter_count_greater_or_equal_than" to "5",
-          "ktlint_function_signature_body_expression_wrapping" to "multiline",
-          "ktlint_standard_comment-wrapping" to "disabled",
-          "max_line_length" to "180",
-          "ktlint_class_signature_rule_force_multiline_when_parameter_count_greater_or_equal_than" to "unset"
-        )
-      )
+      ktfmt().googleStyle().configure {
+        it.setMaxWidth(180)
+        it.setBlockIndent(2)
+        it.setContinuationIndent(2)
+        it.setRemoveUnusedImports(false)
+        it.setTrailingCommaManagementStrategy(KtfmtStep.TrailingCommaManagementStrategy.ONLY_ADD)
+      }
     }
   }
 
